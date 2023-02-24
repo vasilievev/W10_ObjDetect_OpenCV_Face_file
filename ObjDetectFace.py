8449de59078abc7813cfc6c96456a5dba795fe8b
Shaman <95121987+vasilievev@users.noreply.github.com>
2023-02-24 16:56:40 +0300

    Update ObjDetectFace.py

--- ObjDetectFace.py
+++ ObjDetectFace.py
@@ -3,14 +3,15 @@ import cvlib as cv
 import numpy as np
 
 cap=cv2.VideoCapture("YOU_FILE.mp4")
+
 while True:
     ret,frame=cap.read()
     if not ret:
         break
+        
     frame=cv2.resize(frame,(1280,1024))
-    #model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    #faces = model.detectMultiScale(frame)
     faces,confidences=cv.detect_face(frame)
+    
     for face,conf in zip(faces,confidences):
         x,y=face[0],face[1]
         x1,y1=face[2],face[3]
@@ -22,7 +23,9 @@ while True:
         cv2.rectangle(frame,(x,y),(x1,y1),(0,255,0),2)
         cv2.putText(frame,str(label),(x,y),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),2)
     cv2.imshow("Frame",frame)
+    
     if cv2.waitKey(1)&0xFF==27:
         break
+        
 cap.release()
 cv2.destroyAllWindows()
